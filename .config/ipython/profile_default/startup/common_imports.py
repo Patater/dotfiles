211a4de9de47374a8e2a69ea2bf10c728b0b2c4f
Jaeden Amero <jaeden@patater.com>
2014-04-10 08:05:16 -0500

    ipython: Add some common imports

---
+++ .config/ipython/profile_default/startup/common_imports.py
@@ -0,0 +1,13 @@
+# http://blog.safaribooksonline.com/2014/02/27/using-ipython-profiles/
+import os
+import sys
+import re
+from fractions import Fraction
+from datetime import date, datetime, timedelta
+from collections import Counter, defaultdict, namedtuple
+from hashlib import md5, sha1
+from itertools import *
+import json
+import base64
+from requests import get, head, post
+import numpy as np
