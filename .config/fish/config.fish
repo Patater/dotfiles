9e5e09bed9cd5316e5f45533a4c4887e9f0ed8f2
Jaeden Amero <jaeden@patater.com>
2015-08-28 12:17:50 -0500

    fish: Only set PATH and EDITOR in login shells

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
 if status --is-login
   set PATH $PATH /usr/bin /sbin /usr/sbin
-end
-set PATH $HOME/Projects/OS/cross/bin $PATH
-set PATH $HOME/Projects/git/contrib/workdir $PATH
 
-set EDITOR vim
+  set PATH $HOME/Projects/OS/cross/bin $PATH
+  set PATH $HOME/Projects/git/contrib/workdir $PATH
+  set PATH $HOME/bin $PATH
+
+  set EDITOR vim
+end
 
 # Keychain
 set -gx HOSTNAME (hostname)
