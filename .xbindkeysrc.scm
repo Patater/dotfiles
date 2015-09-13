9ff44afce7c392810cdbc4e1bfa1a356918e9268
Jaeden Amero <jaeden@patater.com>
2015-09-13 15:40:09 -0500

    Make volume keys control ALSA master mixer
    This is useful on my Thinkpad X61s, where those keys were mapped
    properly, didn't do anything by default.

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,3 @@
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 2dB+ unmute")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 2dB- unmute")
+(xbindkey '("XF86AudioMute") "amixer set Master toggle")
