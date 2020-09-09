0132720ba06f4ada4facb310f22a85bbb7f927fc
Jaeden Amero <jaeden@patater.com>
2020-09-09 17:44:14 +0100

    git: Add ability to clone from other hosts
    Add ability to clone from either github or gitlab or other hosts with
    similarly shaped URLs.

--- .local/bin/github-clone.sh
+++ .local/bin/github-clone.sh
@@ -13,9 +13,10 @@ fi
 
 url=$(echo $url | cut -d . -f 1,2)
 
+host=$(echo $url | cut -d / -f 3)
 user=$(echo $url | cut -d / -f 4)
 repo=$(echo $url | cut -d / -f 5 | cut -d . -f 1)
-directory=$DEST/github.com/$user
+directory=$DEST/$host/$user
 
 mkdir -p $directory
 echo "git clone --recursive $url.git $directory/$repo"
