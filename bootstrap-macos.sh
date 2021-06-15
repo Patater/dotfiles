5120f50a532e3c45e88e34b3a3c04f353b9c1008
Jaeden Amero <jaeden@patater.com>
2021-06-15 11:37:16 +0100

    mac: Add font-ubuntu-mono

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -3,11 +3,13 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $(HOME)/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+brew tap homebrew/cask-fonts
 brew install \
     autoconf \
     automake \
     cmake \
     emscripten \
+    font-ubuntu-mono \
     freeimage \
     git \
     go \
