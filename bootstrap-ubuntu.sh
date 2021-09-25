5eb4a3ba9cfd2cbfe17944140ae0306255e1c6d3
Jaeden Amero <jaeden@patater.com>
2021-09-25 06:26:08 +0100

    ubuntu: Add initial bootstrap script

---
+++ bootstrap-ubuntu.sh
@@ -0,0 +1,32 @@
+sudo apt install -y \
+    build-essential \
+    chessx \
+    cmake \
+    conky \
+    dosbox \
+    evilwm \
+    fish \
+    gimp \
+    git \
+    git-svn \
+    irssi \
+    iverilog \
+    k3b \
+    k4dirstat \
+    libsdl2-dev \
+    net-tools \
+    ninja-build \
+    python3-pip \
+    python3-virtualenv \
+    qbittorrent \
+    stockfish \
+    subversion \
+    tmux \
+    vim \
+    vlc \
+    weather-util \
+    whois \
+    zfsutils-linux \
+
+pip install --user \
+    youtube-dl \
