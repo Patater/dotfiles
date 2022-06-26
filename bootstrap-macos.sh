4b88d2095b6c1b3e9e55c5aaf8daaf29e68a2bbb
Jaeden Amero <jaeden@patater.com>
2022-06-26 06:09:15 +0100

    mac: Remove font-ubuntu-mono

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -3,7 +3,6 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $(HOME)/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew tap homebrew/cask-fonts
 brew install \
     64tass \
     aria2 \
@@ -18,7 +17,6 @@ brew install \
     dos2unix \
     emscripten \
     ffmpeg \
-    font-ubuntu-mono \
     freeglut \
     freeimage \
     git \
