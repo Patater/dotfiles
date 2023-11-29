f20242ea934eec73a6a99f1599e2f00c73cbbcbe
Jaeden Amero <jaeden@patater.com>
2023-11-29 09:22:01 +0400

    fish: Load ssh keys with ssh-agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
-if status --is-login
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
+if status is-interactive
   set PATH $PATH /usr/bin /sbin /usr/sbin
 
   # perl (e.g. pod2man)
